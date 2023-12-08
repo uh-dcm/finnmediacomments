948aaea10da3fe58ec79dab757e0a0d90fb6202e
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2023-12-08 16:16:57 +0200

    Add 4CAT example CSV coverters

--- src/finnmediacomments/hs.py
+++ src/finnmediacomments/hs.py
@@ -6,8 +6,20 @@ def collect( id ):
     
     data = requests.get( url )
     data = data.json()
-    
+
     return data['comments']
+
+def to_4cat_csv( comments , output ):
+    df = pd.DataFrame( comments ).flatten()
+
+    newdf = pd.DataFrame()
+    newdf['body'] = df['comment']
+    newdf['author'] = df['userIdentity'].apply( lambda userIdentity: userIdentity['displayName'] )
+    newdf['timestamp'] = df['createdAt'].apply(  lambda date: datetime.fromtimestamp(  date/1000 ).strftime("%Y-%m-%d %H:%M:%S") )
+    newdf['id'] = df['id']
+    newdf['thread'] = df['articleId']
+
+    newdf.to_csv( output )
 
 if __name__ == '__main__':
     collect( 2000010042389 )
--- src/finnmediacomments/yle.py
+++ src/finnmediacomments/yle.py
@@ -1,15 +1,30 @@
-import requests
 import json
+from datetime import datetime
+
+import requests
+import pandas as pd
 
 def collect( id ):
     app_key = 'sfYZJtStqjcANSKMpSN5VIaIUwwcBB6D'
     app_id = 'yle-comments-plugin'
     url = f"https://comments.api.yle.fi/v1/topics/{id}/comments/accepted?app_id={app_id}&app_key={app_key}&parent_limit=100"
-    
+
     data = requests.get( url )
     data = data.json()
 
     return data
+
+def to_4cat_csv( comments , output ):
+    df = pd.DataFrame( comments )
+
+    newdf = pd.DataFrame()
+    newdf['body'] = df['content']
+    newdf['author'] = df['author']
+    newdf['timestamp'] = df['createdAt'].apply(  lambda date: datetime.strptime(  date , "%Y-%m-%dT%H:%M:%S%z" ).strftime("%Y-%m-%d %H:%M:%S") )
+    newdf['id'] = df['id']
+    newdf['thread'] = df['topicExternalId']
+
+    newdf.to_csv( output )
 
 if __name__ == '__main__':
     print( len( collect( '74-20063567' ) ) )
